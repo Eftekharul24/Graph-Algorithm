e66ee3af3126f4c6257f4c7b19f390f991c32f2d
MD. Eftekharul Islam <144184559+Eftekharul24@users.noreply.github.com>
2023-10-09 16:26:16 +0600

    Create Breadth-First Search (BFS).java

---
+++ Breadth-First Search (BFS).java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
+
+class Graph {
+    private int V; // Number of vertices
+    private List<List<Integer>> adj; // Adjacency list representation
+
+    public Graph(int V) {
+        this.V = V;
+        adj = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj.get(v).add(w);
+    }
+
+    public void BFS(int startVertex) {
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.poll();
+            System.out.print(currentVertex + " ");
+
+            for (Integer neighbor : adj.get(currentVertex)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(7); // Create a graph with 7 vertices
+
+        // Add edges
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+        g.addEdge(2, 6);
+
+        System.out.print("BFS starting from vertex 0: ");
+        g.BFS(0);
+    }
+}
